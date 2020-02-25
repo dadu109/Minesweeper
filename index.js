88fd9b07f0bb89d753347c7c83865dd780ebd54e
dadu109 <31367753+dadu109@users.noreply.github.com>
2020-02-25 14:28:59 +0100

    Sidebar addition && code cleanup

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const root = document.querySelector('.root')
+const restartButton = document.querySelector('.restart')
+const allDiff = document.querySelectorAll('.diff')
+const timerDisplay = document.querySelector('.timer')
+const difficulties = {
+    "easy":{n:10,m:20},
+    "mid":{n:17,m:40},
+    "hard":{n:25,m:75}
+}
+let globalGridSize = difficulties.mid.n;
+let globalMinesAmount = difficulties.mid.m;
+let time = 0;
+
+function pickDiff(t){
+    const diff = t.target.dataset.diff;
+    allDiff.forEach(e=>{e.dataset.diff===diff?e.classList.add('active'):e.classList.remove('active')})
+    globalGridSize=difficulties[diff].n;
+    globalMinesAmount=difficulties[diff].m;
+    restartGame();
+}
+
 function generateGrid (gridSize,mines){
     const minesIndex = [...Array(mines).keys()].map((e,i,a)=>{
         return Math.floor(Math.random() * (gridSize*gridSize))
@@ -45,31 +66,30 @@ function fillMineAroundNumber(gridWithMines){
     return gwm;
 }
 
-function renderGrid(dimention){
-    const generated = generateGrid(dimention,dimention*4);
+function renderGrid(dimention,mines){
+    const generated = generateGrid(dimention,mines);
 
     for (let i = 0; i < generated.length; i++) {
         const row = document.createElement('div')
         row.classList.add('row')
-        document.body.appendChild(row);
+        root.appendChild(row);
         for (let j = 0; j < generated.length; j++) {
             const cell = document.createElement('div')
             cell.dataset.id=generated[i][j].id;
             cell.classList.add('cell')
             cell.classList.add('cover')
-            cell.addEventListener('click',
-                (e)=>{
-                    removeCover(e.target,generated)
-                }
-            )
+            cell.addEventListener('click',(e)=>{removeCover(e.target,generated)})
+
             cell.addEventListener('contextmenu',(e)=>{
+                const {target:{classList,dataset}} = e;
                 e.preventDefault();
-                e.target.dataset.tagged=="true"?e.target.classList.remove('tagged'):e.target.classList.add('tagged');
-                e.target.dataset.tagged==="true"?e.target.dataset.tagged="false":e.target.dataset.tagged="true";
+                dataset.tagged=="true"?classList.remove('tagged'):classList.add('tagged');
+                dataset.tagged==="true"?dataset.tagged="false":dataset.tagged="true";
             })
+
             generated[i][j].mine?cell.classList.add('mine'):null;
             if(generated[i][j].minesAround>0){
-                cell.textContent=generated[i][j].minesAround;
+                cell.textContent = generated[i][j].minesAround;
                 cell.classList.add(`color-${generated[i][j].minesAround}`)
             };
             if(generated[i][j].mine){cell.textContent='💣'};
@@ -81,26 +101,24 @@ function renderGrid(dimention){
 
 function removeCover(t,grid){
     if(!t.dataset.clicked==true){
+        const dcs = (a,b) =>(document.querySelector(`[data-id="${grid[a][b].id}"]`))
+        const [j,i] = grid.reduce((p,q)=>{
+            const arr = q.filter(e=>e.id==t.dataset.id);
+            return arr.length>0?[arr[0].x,arr[0].y]:p
+        },[]) //gets the position of target in overall grid
+
         t.dataset.clicked=true;
         t.classList.remove('cover')
-        const dcs = (a,b) =>{
-            return document.querySelector(`[data-id="${grid[a][b].id}"]`)
-        };
-        const [j,i] = grid.reduce((p,q)=>
-            {
-                let arr = q.filter(e=>e.id==t.dataset.id);
-                if(arr.length>0){return [arr[0].x,arr[0].y]} else return p
-            },[])
         if(grid[i][j].mine){gameLost(grid)}
         if(grid[i][j].minesAround===0){
-            if(grid[i-1]    && grid[i-1][j-1] && !grid[i-1][j-1].mine){removeCover(dcs(i-1,j-1),grid)}
-            if(grid[i-1]    && grid[i-1][j  ] && !grid[i-1][j  ].mine){removeCover(dcs(i-1,j  ),grid)}
-            if(grid[i-1]    && grid[i-1][j+1] && !grid[i-1][j+1].mine){removeCover(dcs(i-1,j+1),grid)}
-            if(grid[i][j-1] &&                   !grid[i  ][j-1].mine){removeCover(dcs(i  ,j-1),grid)}
-            if(grid[i][j+1] &&                   !grid[i  ][j+1].mine){removeCover(dcs(i  ,j+1),grid)}
-            if(grid[i+1]    && grid[i+1][j-1] && !grid[i+1][j-1].mine){removeCover(dcs(i+1,j-1),grid)}
-            if(grid[i+1]    && grid[i+1][j  ] && !grid[i+1][j  ].mine){removeCover(dcs(i+1,j  ),grid)}
-            if(grid[i+1]    && grid[i+1][j+1] && !grid[i+1][j+1].mine){removeCover(dcs(i+1,j+1),grid)}
+            if(grid[i-1]    && grid[i-1][j-1] && !grid[i-1][j-1].mine){ removeCover(dcs(i-1,j-1) ,grid)}
+            if(grid[i-1]    && grid[i-1][j  ] && !grid[i-1][j  ].mine){ removeCover(dcs(i-1,j  ) ,grid)}
+            if(grid[i-1]    && grid[i-1][j+1] && !grid[i-1][j+1].mine){ removeCover(dcs(i-1,j+1) ,grid)}
+            if(grid[i][j-1] &&                   !grid[i  ][j-1].mine){ removeCover(dcs(i  ,j-1) ,grid)}
+            if(grid[i][j+1] &&                   !grid[i  ][j+1].mine){ removeCover(dcs(i  ,j+1) ,grid)}
+            if(grid[i+1]    && grid[i+1][j-1] && !grid[i+1][j-1].mine){ removeCover(dcs(i+1,j-1) ,grid)}
+            if(grid[i+1]    && grid[i+1][j  ] && !grid[i+1][j  ].mine){ removeCover(dcs(i+1,j  ) ,grid)}
+            if(grid[i+1]    && grid[i+1][j+1] && !grid[i+1][j+1].mine){ removeCover(dcs(i+1,j+1) ,grid)}
         }
     }
 }
@@ -114,6 +132,36 @@ function gameLost(grid){
             e.dataset.clicked=true;
         }
     }
+    clearInterval(timer)
 }
 
-renderGrid(24);
+function restartGame(){
+    root.innerHTML='';
+    renderGrid(globalGridSize,globalMinesAmount);
+    restartTime();
+}
+
+
+function setTimer(){
+    let minutes = Math.floor(time / 60);
+    let seconds = time - minutes * 60;
+    timerDisplay.innerHTML=`${minutes<10?"0"+minutes:minutes}:${seconds<10?"0"+seconds:seconds}`
+    time++;
+}
+
+const restartTime = () => {
+    time=0;
+    clearInterval(timer);
+    timer = setInterval(()=>{setTimer()},1000);
+};
+
+let timer = setInterval(()=>{setTimer()},1000);
+restartButton.addEventListener('mousedown',(e)=>{e.target.classList.add('restart-pressed')})
+restartButton.addEventListener('mouseup',(e)=>{
+    e.target.classList.remove('restart-pressed');
+    restartGame();
+    restartTime()
+});
+allDiff.forEach(e=>{e.addEventListener('click',(t)=>{pickDiff(t)})})
+
+renderGrid(globalGridSize,globalMinesAmount);
